0517040e0519daa97fa0143d9270f9f3b70a47b4
Parsa Mohammadian <parsa2820@gmail.com>
2023-05-03 18:47:58 +0330

    Reduce number of cores in 1

--- 2/practical/1.sh
+++ 2/practical/1.sh
@@ -2,7 +2,7 @@
 
 gcc -o 1 1.c -lpthread
 
-for i in {1..7}
+for i in {1..2}
 do
     echo "Running 1 with $i threads"
     perf stat --event page-faults,cache-misses,dTLB-load-misses,dTLB-loads,dTLB-stores ./1 $i
